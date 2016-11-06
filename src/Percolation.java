4c875dee304a360a089200e31fff9ac9f9c3d1ca
Cooper Bell <john.cooper.bell@gmail.com>
2016-11-06 15:59:42 -0600

    Finally solve problem

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,48 +1,95 @@
-import edu.princeton.cs.algs4.UF;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-    private boolean[][] grid;
+    private boolean[][] blocked;
     private final int gridSize;
-    private final UF uf;
+    private final WeightedQuickUnionUF uf;
+
+    private final int topId;
+    private final int bottomId;
 
     public Percolation(int n) {
-        gridSize = n;
-        grid = new boolean[n][n];
-        uf = new UF(n);
+        if (n <= 0)
+            throw new IllegalArgumentException("n cannot be less than or equal to 0");
 
-        // Initialize grid squares to be completely filled (true means filled)
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
-                grid[i][j] = true;
+        gridSize = n;
+        blocked = new boolean[n+1][n+1];
+        uf = new WeightedQuickUnionUF((n+1) * (n+1) + 1);
+
+        // Since min(id) == gridSize + 1 and max(id) == gridSize * (gridSize + 1)
+        // init top/bottom ids outside of these ranges
+        topId = 0;
+        bottomId = (n+1) * (n+1);
+
+        // Initialize blocked squares to be completely filled (true means filled)
+        for (int i = 1; i <= n; i++)
+            for (int j = 1; j <= n; j++)
+                blocked[i][j] = true;
+    }
+
+    private int id(int i, int j) {
+        return j * gridSize + i;
     }
 
     public void open(int row, int col) {
         checkDimensions(row, col);
-        grid[row][col] = false;
+
+        // If cell is already unblocked, return
+        if (isOpen(row, col))
+            return;
+
+        // Open the current cell
+        blocked[row][col] = false;
+
+        // up, right, left, right cells
+        int[][] tups = new int[][] {
+                {row - 1, col}, {row, col - 1},
+                {row, col + 1}, {row + 1, col} };
+
+        // Connect with top/bottom id if on the top or bottom row
+        if (row == 1)
+            uf.union(id(row, col), topId);
+        else if (row == gridSize)
+            uf.union(id(row, col), bottomId);
+
+        // Check the surrounding cells and connect the open ones
+        for (int[] t : tups) {
+            if (inBounds(t[0], t[1]) && isOpen(t[0], t[1]))
+                uf.union(id(row, col), id(t[0], t[1]));
+
+            // Connect with top/bottom id if on the top or bottom row
+            if (t[0] == 1)
+                uf.union(id(t[0], t[1]), topId);
+            else if (t[0] == gridSize)
+                uf.union(id(t[0], t[1]), bottomId);
+        }
+    }
+
+    private boolean inBounds(int row, int col) {
+        return row >= 1 && col >= 1 && row <= gridSize && col <= gridSize;
     }
 
     public boolean isOpen(int row, int col) {
         checkDimensions(row, col);
-        return !isFull(row, col);
+        return !blocked[row][col];
     }
 
     public boolean isFull(int row, int col) {
         checkDimensions(row, col);
-        return grid[row][col];
-    }
-
-    private void checkDimensions(int row, int col) {
-        if (row < 0 || col < 0)
-            throw new IllegalArgumentException("Argument cannot be negative!");
-        if (row > gridSize || col > gridSize)
-            throw new IndexOutOfBoundsException("Cannot exceed grid size of " + gridSize + "!");
+        return isOpen(row, col) && uf.connected(id(row, col), topId);
     }
 
     public boolean percolates() {
-        return false;
+        return uf.connected(topId, bottomId);
+    }
+
+    private void checkDimensions(int row, int col) {
+        if (row < 1 || col < 1)
+            throw new IndexOutOfBoundsException("Argument cannot be negative!");
+        if (row > gridSize || col > gridSize)
+            throw new IndexOutOfBoundsException("Cannot exceed blocked size of " + gridSize + "!");
     }
 
     public static void main(String[] args) {
-
     }
 }
--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -1,24 +1,65 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
 public class PercolationStats {
+    private double[] trialRuns;
+    private int trials;
+
     public PercolationStats(int n, int trials) {
+        if (n <= 0 || trials <= 0)
+            throw new IllegalArgumentException("n and trials must be positive integers");
+
+        this.trials = trials;
+
+        trialRuns = new double[trials];
+        for (int i = 0; i < trials; i++)
+            trialRuns[i] = runPercolation(n);
+    }
+
+    private double runPercolation(int n) {
+        Percolation perc = new Percolation(n);
+        int opened = 0;
+        boolean[][] openCells = new boolean[n+1][n+1];
+        while (!perc.percolates()) {
+            int row = StdRandom.uniform(1, n + 1);
+            int col = StdRandom.uniform(1, n + 1);
+            if (!openCells[row][col]) {
+                perc.open(row, col);
+                openCells[row][col] = true;
+                opened++;
+            }
+        }
+        return ((double) opened) / (n * n);
     }
 
     public double mean() {
-        return 0.0;
+        return StdStats.mean(trialRuns);
     }
 
     public double stddev() {
-        return 0.0;
+        return StdStats.stddev(trialRuns);
     }
 
     public double confidenceLo() {
-        return 0.0;
+        double std = stddev();
+        double mean = mean();
+        return mean - (1.96 * std / Math.sqrt(trials));
     }
 
     public double confidenceHi() {
-        return 0.0;
+        double std = stddev();
+        double mean = mean();
+        return mean + (1.96 * std / Math.sqrt(trials));
     }
 
     public static void main(String[] args) {
-
+        int n = StdIn.readInt();
+        int trials = StdIn.readInt();
+        PercolationStats stats = new PercolationStats(n, trials);
+        StdOut.println("mean: " + stats.mean());
+        StdOut.println("stddev: " + stats.stddev());
+        StdOut.println("95% confidence interval: [" + stats.confidenceLo() + ", " + stats.confidenceHi() + "]");
     }
 }
